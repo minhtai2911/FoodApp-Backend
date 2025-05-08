fe4485ebd176afede28be2ea76a4b3d14b3e12bc
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2025-05-08 23:37:59 +0700

    fix userAddress

--- src/controllers/userAddressController.js
+++ src/controllers/userAddressController.js
@@ -4,7 +4,8 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import logger from "../utils/logger.js";
 
 const createUserAddress = asyncHandler(async (req, res, next) => {
-  const { userId, city, district, commune, street, phone } = req.body;
+  const { city, district, commune, street, phone } = req.body;
+  const userId = req.user.id;
 
   if (!city || !district || !commune || !street || !phone) {
     logger.warn(messages.MSG1);
@@ -54,8 +55,38 @@ const getUserAddressById = asyncHandler(async (req, res, next) => {
   res.status(200).json({ data: userAddress });
 });
 
+const getUserAddressByUserId = asyncHandler(async (req, res, next) => {
+  const userId = req.user.id;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  const totalCount = await UserAddress.countDocuments({ userId });
+  const userAddresses = await UserAddress.find({ userId })
+    .skip(skip)
+    .limit(limit)
+    .exec();
+
+  if (!userAddresses || userAddresses.length === 0) {
+    logger.warn("Địa chỉ giao hàng không tồn tại");
+    return res.status(404).json({ error: "Not found" });
+  }
+
+  logger.info("Lấy địa chỉ giao hàng thành công!");
+  res.status(200).json({
+    meta: {
+      meta: {
+        totalCount: totalCount,
+        currentPage: page,
+        totalPages: Math.ceil(totalCount / limit),
+      },
+      data: userAddresses,
+    },
+  });
+});
+
 export default {
   createUserAddress: createUserAddress,
   updateUserAddressById: updateUserAddressById,
   getUserAddressById: getUserAddressById,
+  getUserAddressByUserId: getUserAddressByUserId,
 };
--- src/routes/userAddressRoute.js
+++ src/routes/userAddressRoute.js
@@ -4,8 +4,9 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/", userAddressController.createUserAddress);
+router.post("/", authMiddleware.verifyToken,userAddressController.createUserAddress);
 router.put("/:id" , authMiddleware.verifyToken, userAddressController.updateUserAddressById);
 router.get("/:id", authMiddleware.verifyToken, userAddressController.getUserAddressById);
+router.get("/", authMiddleware.verifyToken, userAddressController.getUserAddressByUserId);
 
 export default router;
